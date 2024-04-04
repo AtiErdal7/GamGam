57bfb8a9a4aeabd7ff382039a6ec711d35085387
AtiErdal7 <atakan@multiplayer.com.tr>
2024-04-04 12:45:45 +0300

    testttt

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,12 +2,12 @@ const Navbar = () => {
     return(
         <nav className="navbar">
             <div className="content">
-                <a href="/collect-ticket"style={{
+                <a href="/test2/collect-ticket"style={{
                     color: "white",
                     backgroundColor:"grey",
                     borderRadius: "8px"
                 }}>Click Page</a>
-                <a href="/sign-in Rewards" style={{
+                <a href="/test2/sign-in Rewards" style={{
                     color: "gold",
                     backgroundColor:"green",
                     borderRadius: "8px"
